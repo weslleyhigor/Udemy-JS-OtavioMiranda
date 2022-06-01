39892c1abf10a792b7fd818703708609650a52e1
Weslley Higor <weshigor@gmail.com>
2022-06-01 13:33:55 -0500

    subindo arquivo da aulo de js

---
+++ Logica-de-programacao/operacao-ternaria.js
@@ -0,0 +1,9 @@
+const pontuacaoUsuario = 500;
+const nivelUsuario = pontuacaoUsuario >= 1000? 'Usuário VIP' : 'Usuário Normal';
+
+const corUsuario = null;
+const corPadrao = corUsuario || 'Preta';
+
+console.log(nivelUsuario);
+
+// TODO O IF EM UMA SÓ LINHA;
