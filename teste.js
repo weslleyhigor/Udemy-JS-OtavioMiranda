16692464781376aa6b49d18b4cfd0b82e3db3f3d
Weslley Higor Campos Ferreira <weslley.ferreira@grupoboticario.com.br>
2022-07-13 16:45:42 -0300

    teste de codigo pra aux desenvolve

--- teste.js
+++ teste.js
@@ -1,6 +1,6 @@
-//                      0   1       2      3    4   5
-const listNumbers = [ 1000, 2000, 3000, 4000, 5000, 7000];
+const frutas = ['Maça','Pera', 'Uva', 'Morango'];
+const frutasAleatorias = [];
 
-[indZero, , indDois, , indQuatro ] = listNumbers;
-
-console.log(indZero, indDois, indQuatro); 
+for (let i = 0; i < frutas.length; i++) {
+    let numSorted;
+}
