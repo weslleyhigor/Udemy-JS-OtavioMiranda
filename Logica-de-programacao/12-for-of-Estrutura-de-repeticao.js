6051f08fa72b8fd404e2c18fa6020fcf6612df27
Weslley Higor <weshigor@gmail.com>
2022-07-21 09:01:19 -0500

    subindo alteraçoes dos arquivos

--- Logica-de-programacao/12-for-of-Estrutura-de-repeticao.js
+++ Logica-de-programacao/12-for-of-Estrutura-de-repeticao.js
@@ -1,10 +1,13 @@
 // Indices    0123456789...
 const nome = 'Weslley Higor';
 
+// for classico
 for ( let i = 0; i < nome.length; i++) {
     console.log(nome[i]);
 };
 
+
+// for usando in
 for ( let i in nome) {
     console.log(nome[i]);
 }
--- teste.js
+++ teste.js
@@ -1,6 +1,19 @@
-const frutas = ['Maça','Pera', 'Uva', 'Morango'];
-const frutasAleatorias = [];
+// Função para randomizar array
+function baguncaArray(arr) {
+    // Loop em todos os elementos
+    for (let i = arr.length - 2; i > 0; i--) {
+            // Escolhendo elemento aleatório
+        const sorteiaNumero = Math.floor(Math.random() * (i + 1));
+        // Reposicionando elemento
+        [arr[i], arr[sorteiaNumero]] = [arr[sorteiaNumero], arr[i]];
+    }
+    
+    // Retornando array com aleatoriedade
+    return arr;
+};
 
-for (let i = 0; i < frutas.length; i++) {
-    let numSorted;
-}
+const numeros = [1, 2, 3, 4];
+
+console.log(baguncaArray(numeros)); 
+console.log(baguncaArray(numeros)); 
+console.log(baguncaArray(numeros)); 
