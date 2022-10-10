d756674a93818ea7d535f9b01d9af16416aacfad
Weslley Higor <weshigor@gmail.com>
2022-10-10 09:53:40 -0500

    subindo diretorio renomeado do curso de javaScript

---
+++ 3-JavaScript-Funcoes-Avancado/1-funcoes.js
@@ -0,0 +1,28 @@
+// Declaração de função (Function hoisting)
+function falaOi(){
+    console.log('Oi');
+}
+falaOi();
+
+// As funções são First-class objects(Objetos de primeira classe)
+// As funções podem ser tratadas como dados/Expressões.
+// Function expression
+
+const souUmDado = function(){
+    console.log('Sou um dado');
+};
+souUmDado();
+
+// Arrow function
+const funcaoArrou = () => {
+    console.log('Sou uma arrow function')
+};
+funcaoArrou();
+
+// Dentro de um objeto
+const obj = {
+    falar(){
+        console.log('Estou falando...');
+    }
+};
+obj.falar();
